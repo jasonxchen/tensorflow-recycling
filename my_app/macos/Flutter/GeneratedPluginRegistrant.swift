eff01465078e963f32b3be3b272082dfd87a59e2
JasonXChen <jasonxchen888@gmail.com>
2022-10-30 17:40:58 -0400

    Store TFLite flutter interpreter and allow taking pictures

--- my_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ my_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,8 +7,10 @@ import Foundation
 
 import firebase_core
 import firebase_ml_model_downloader
+import path_provider_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FirebaseModelDownloaderPlugin.register(with: registry.registrar(forPlugin: "FirebaseModelDownloaderPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
