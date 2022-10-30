0c0607ba350f19d60d4e1cc1ec1603568e94e4be
JasonXChen <jasonxchen888@gmail.com>
2022-10-30 01:47:41 -0400

    Add Firebase ML Model Downloader plugin

--- my_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ my_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import firebase_core
+import firebase_ml_model_downloader
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FirebaseModelDownloaderPlugin.register(with: registry.registrar(forPlugin: "FirebaseModelDownloaderPlugin"))
 }
